ee6113e11b57f3c85e5aafd466babede6fdf0870
Javier Almarza <javieralmarzabucarey@gmail.com>
2024-10-15 21:51:40 -0300

    :bento: se agregan iconos

---
+++ src/assets/index.ts
@@ -0,0 +1,16 @@
+// 🛢️ Barrel.
+import BriefcaseIcon from './Briefcase.astro'
+import LinkedInIcon from './LinkedIn.astro'
+import GitHubIcon from './GitHub.astro'
+import MailIcon from './Mail.astro'
+import CalendarIcon from './Calendar.astro'
+
+// Export 🐶.
+export * from './'
+export {
+  BriefcaseIcon,
+  LinkedInIcon,
+  GitHubIcon,
+  MailIcon,
+  CalendarIcon
+}
